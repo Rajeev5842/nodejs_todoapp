aeb461d88eecbead84188fd69c98a4ad6dc8d7d7
RajeevDangwal <rajeev.hmdangwal@gmail.com>
2024-01-26 08:53:23 +0530

    initial commit

---
+++ middlewares/error.js
@@ -0,0 +1,19 @@
+class ErrorHandler extends Error{
+    constructor(message,statuscode){
+        super(message);
+        this.statuscode = statuscode;
+    }
+}
+
+export const errorMiddleware=(err,req,res,next)=>{
+
+    err.message=err.message || "Internal server error";
+    err.statusCode=err.statusCode || 500;
+
+    return res.status(err.statusCode).json({
+        success:false,
+        message:err.message,
+    }) 
+}
+
+export default ErrorHandler;
